458edcfb0a52143ab41757f9dc91a1f580d85860
Harlan T Wood <code@harlantwood.net>
2017-03-06 18:18:11 -0800

    whitespace

--- ui/hc.js
+++ ui/hc.js
@@ -1,15 +1,13 @@
-
-     function send() {
-         $.post(
-             "/fn/"+$('select[name=zome]').val()+"/"+$('select[name=fn]').val(),
-             $('#data').val(),
-             function(data) {
-                 $("#result").html("result:"+data)
-                 $("#err").html("")
-             }
-         ).error(function(response) {
-             $("#err").html(response.responseText)
-             $("#result").html("")
-         })
-         ;
-     };
+function send() {
+    $.post(
+        "/fn/" + $('select[name=zome]').val() + "/" + $('select[name=fn]').val(),
+        $('#data').val(),
+        function(data) {
+            $("#result").html("result:" + data)
+            $("#err").html("")
+        }
+    ).error(function(response) {
+        $("#err").html(response.responseText)
+        $("#result").html("")
+    });
+};
