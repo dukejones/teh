da9368da8b2209c2799d31440bb27d0c2ea613c8
Harlan T Wood <code@harlantwood.net>
2017-03-06 19:07:21 -0800

    set up JS linting

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+const config = {
+  extends: 'comakery'
+}
+
+module.exports = config
--- zome_jsZome.js
+++ zome_jsZome.js
@@ -2,15 +2,19 @@
 // function getProperty(x) {return property(x)};
 
 expose("claim", HC.JSON);
-
 function claim(x) {
-    return commit("trust_atom", x);
+  return commit("trust_atom", x);
+}
+
+expose("get", HC.JSON);
+function get(x) {
+
 }
 
 function validate(entry_type, entry, props) {
-    return true
+  return true
 }
 
 function genesis() {
-    return true
+  return true
 }
